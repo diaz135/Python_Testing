9a8c2debe0aee7cf48137f6759304b65e476b186
diaz135 <ouattarasamira225@icloud.com>
2024-10-01 08:43:18 +0000

    le probleme de prevention d'email incorect

--- server.py
+++ server.py
@@ -32,12 +32,23 @@ clubs = loadClubs()
 def index():
     return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST'])
+@app.route('/showSummary', methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    email = request.form['email'] 
+    club = next((club for club in clubs if club['email'] == email), None)
+    
+    if club:
+      
+        return render_template('welcome.html', club=club, competitions=competitions)
+    else:
+       
+        flash("L'email saisi n'est pas reconnu. Veuillez essayer à nouveau.")
+        return redirect(url_for('index')) 
 
 
+
+
+   
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
     foundClub = [c for c in clubs if c['name'] == club][0]
